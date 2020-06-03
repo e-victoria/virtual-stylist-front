41e8bb19e0503b57796cb70c321993a4c87bbdfb
e-victoria <evictoria@protonmail.com>
2020-06-03 13:52:15 +0200

    Change options from input to select on btn click

--- src/app/wardrobe/item-details.component.ts
+++ src/app/wardrobe/item-details.component.ts
@@ -3,6 +3,8 @@ import IClothes from './item-detail.model';
 import { faPen } from '@fortawesome/free-solid-svg-icons';
 import {ItemDetailService} from "./item-details.service";
 import {Router} from "@angular/router";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {CreateFormService} from "../create-form/create-form.service";
 
 @Component({
   selector: 'app-item-details',
@@ -13,12 +15,45 @@ export class ItemDetailsComponent implements OnInit {
 
   item: IClothes;
   faPen = faPen;
+  isSubmitted: boolean = false;
+  sizeOptions: string[];
+  colorOptions: string[];
+  clothTypeOptions: string[];
 
-  constructor(private itemDetailService: ItemDetailService, private router: Router) { }
+  editForm: FormGroup = new FormGroup({
+    clothType: new FormControl('', [
+      Validators.required
+    ]),
+    size: new FormControl(''),
+    color: new FormControl('', [
+      Validators.required
+    ]),
+    brand: new FormControl(''),
+    shopLink: new FormControl(''),
+    tag: new FormControl(''),
+    code: new FormControl('')
+  });
+
+  constructor(private itemDetailService: ItemDetailService, private router: Router, private createFormService: CreateFormService) { }
 
   ngOnInit(): void {
     const itemId = this.router.url.split('/').pop();
     this.getItemData(itemId);
+    this.getSelectOptions();
+  }
+
+  get clothType() {
+    return this.editForm.get('clothType');
+  }
+
+  get color(){
+    return this.editForm.get('color');
+  }
+
+  getSelectValue(event) {
+    const selectedValue = event[0];
+    const inputName = event[1];
+    this.editForm.get(inputName).setValue(selectedValue.toString().toUpperCase());
   }
 
   getItemData(id: string) {
@@ -33,7 +68,26 @@ export class ItemDetailsComponent implements OnInit {
     event.stopPropagation();
     event.preventDefault();
     const input = event.currentTarget.parentNode.querySelector('.item__details-content');
+    const label = event.currentTarget.parentNode.querySelector('.item-details__label');
+    label.classList.toggle('hide');
+    event.currentTarget.parentNode.querySelector('.item-details__select').classList.toggle('show-flex');
     input.removeAttribute('disabled');
     input.classList.add('item__details-content--active');
+  }
+
+  getSelectOptions() {
+    this.createFormService.getSelectOptions().subscribe({
+      next: options => {
+        this.sizeOptions = options['Size'];
+        this.colorOptions = options['Color'];
+        this.clothTypeOptions = options['ClothType'];
+      }
+    });
+  }
+
+  saveChanges(event) {
+    event.preventDefault();
+
+    this.isSubmitted = true;
   }
 }
